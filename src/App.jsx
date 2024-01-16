909efce0df3ce27e39f228e5b8b1b0693dba41eb
silvaPool <silva.friends22@gmail.com>
2024-01-16 14:31:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+   let faceio;
+   useEffect(() => {
+    faceio = new faceIO("fioa8516");
+    }, []);
+ 
+    const handleSignIn = async () => {
+      try {
+        let response = await faceio.enroll({
+          locale: "auto",
+          payload: {
+            email: "example@gmail.com",
+            pin: "12345",
+          },
+        });
+
+        console.log(`Unique Facial ID: ${response.facialId}
+        Enrollment Date: ${response.timestamp}
+        Gender: ${response.details.gender}
+        Age Approximation: ${response.details.age}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const handleLogIn = async () => {
+      try {
+        let response = await faceio.authenticate({
+          locale: "auto",
+        });
+
+        console.log(`Unique Facial ID: ${response.facialId}
+          PayLoad: ${response.payload}
+        `)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+  return (
+
+    <>
+
+
+
+  <section>
+    <h1>Face Authentication by FaceIO</h1>
+    <button onClick={handleSignIn}>Sign-in</button>
+    <button onClick={handleLogIn}>Log-in</button>
+  </section>
+
+  </>
+  )
+}
+
+export default App
